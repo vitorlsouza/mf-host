7c71d2919946813bb163852217e7646bd3defe99
Vitor <vitorlopesdesouza@gmail.com>
2025-06-26 10:21:43 -0400

    first commit

---
+++ module-federation.config.ts
@@ -0,0 +1,10 @@
+export const mfConfig = {
+  name: "host",
+  filename: "remoteEntry.js",
+  remotes: {
+    remote:
+      "remote@https://t-latest-remote-mf-app-vitorlsouza-ze.zephyrcloud.app/remoteEntry.js",
+  },
+  exposes: {},
+  shared: ["react", "react-dom"],
+};
